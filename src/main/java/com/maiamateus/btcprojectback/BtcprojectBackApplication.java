6f56dcefca554f38433fbb54fce4c43f7ca2262e
Mateus Otavio <maiamateusotavio@gmail.com>
2021-02-03 20:28:15 -0300

    Projeto iniciado

---
+++ src/main/java/com/maiamateus/btcprojectback/BtcprojectBackApplication.java
@@ -0,0 +1,13 @@
+package com.maiamateus.btcprojectback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BtcprojectBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BtcprojectBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maiamateus/btcprojectback/BtcprojectBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maiamateus.btcprojectback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BtcprojectBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
